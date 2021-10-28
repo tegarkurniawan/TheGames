2d9ed85e295cd994d99e72788fd8cf08e86525bb
tegarkurniawan <tegarkurniawan145@gmail.com>
2021-10-28 12:08:03 +0700

    Initial Commit

---
+++ TheGamesApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheGamesApps
+//
+//  Created by tegar kurniawan on 28/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
